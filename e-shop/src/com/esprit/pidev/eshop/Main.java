7c1564479bdcef77140522784f8063401d75cc06
Syrine <syrine.bouslimi@esprit.tn>
2017-04-04 23:57:32 +0200

    Committed Hello World of e-SHOP

---
+++ e-shop/src/com/esprit/pidev/eshop/Main.java
@@ -0,0 +1,11 @@
+package com.esprit.pidev.eshop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
